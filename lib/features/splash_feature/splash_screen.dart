b4d274b2ca44c3b2edfb575742861b9695e4483c
fppinedajr <fernandojr03.pineda@gmail.com>
2026-02-13 13:00:59 +0800

    Design splash screen

--- lib/features/splash_feature/splash_screen.dart
+++ lib/features/splash_feature/splash_screen.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    return Scaffold(
+      body: Column(
+        spacing: 20,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Image.asset('assets/shape.png'),
+          ),
+          Image.asset('assets/done_checking.png'),
+
+          Text(
+            "Gets thing with TODs",
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: Text(
+              "Lorem ipsum dolor sit amet consectetur. Eget sit nec et euismod. Consequat urna quam felis interdum quisque. Malesuada adipiscing tristique ut eget sed.",
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          Spacer(),
+          SizedBox(
+            height: 60,
+            width: 360,
+            child: ElevatedButton(
+              onPressed: () {
+                print("Button Pressed!");
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Color(0xDD50C2C9)),
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              child: Text(
+                "Get Started",
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 60),
+        ],
+      ),
+    );
   }
 }
-
-
